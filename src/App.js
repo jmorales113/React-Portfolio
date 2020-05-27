bd7e09211c78a669749fc1d4e116945be66d2ff7
jmorales113 <jmorales113@gmail.com>
2020-05-27 16:12:37 -0700

    Footer component compiling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Portfolio from "./components/Portfolio";
 import Nav from "./components/Nav";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Portfolio />
       <Education />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="copyright">
+      <p>© 2020 Jorge Morales (818) 644-1942 jmorales113@gmail.com</p>
+    </footer>
+  );
+}
+
+export default Footer;
